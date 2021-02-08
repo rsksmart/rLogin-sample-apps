786a91e84520ad94fb9cfd74a34d1155ed0d3fd3
Jesse Clark <hello@developerjesse.com>
2021-02-08 13:35:05 +0200

    add packagejson and add very basic backend app.

---
+++ permissioned-app/backend/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const didAuth = require('@rsksmart/express-did-auth')
+const { SimpleSigner } = require('did-jwt')
+
+// This private key must not be used in production. This is just a sample application.
+// Never use harcoded private keys in your project.
+const privateKey = '72e7d4571572838d3e0fe7ab18ea84d183beaf3f92d6c8add8193b53c1a542a2'
+const serviceDid = 'did:ethr:rsk:0x45eDF63532b4dD5ee131e0530e9FB12f7DA1915c'
+const serviceSigner = SimpleSigner(privateKey)
+const challengeSecret = 'secret-pass'
+const serviceUrl = 'http://localhost:3007'
+
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.json())
+
+const authMiddleware = didAuth.default({
+  serviceDid,
+  serviceSigner,
+  serviceUrl,
+  challengeSecret,
+  requiredCredentials: ['Email'],
+  requiredClaims: ['Name'],
+  signupBusinessLogic: (payload) => { console.log(payload); return true }
+})(app)
+
+app.use(authMiddleware)
+
+const port = 3007
+
+app.listen(port, () => console.log(`Backend started at http://localhost:${port}`))
