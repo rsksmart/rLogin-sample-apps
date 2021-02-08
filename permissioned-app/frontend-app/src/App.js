92e0d582f7e85a90fcd28f0d4ed490d598738fda
Jesse Clark <hello@developerjesse.com>
2021-02-08 13:45:22 +0200

    Remove thing we don't need.

--- permissioned-app/frontend-app/src/App.js
+++ permissioned-app/frontend-app/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        rLogin Sample App - Permissioned App
       </header>
+
+      <section id="connect">
+        <h2>Start here!</h2>
+        <button>Login!</button>
+      </section>
     </div>
   );
 }
