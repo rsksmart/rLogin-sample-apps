49e6959a180d08fb1b204b7a21404f289776e38d
Jesse Clark <hello@developerjesse.com>
2021-02-12 14:18:03 +0200

    Update first test and fix issue with TextEncoder.

--- basic-dapp/src/App.test.js
+++ basic-dapp/src/App.test.js
@@ -2,7 +2,7 @@ import { mount } from 'enzyme'
 import App from './App';
 
 describe('Component: App', () => {
-  test('renders learn react link', () => {
+  test('renders component and has header', () => {
     const component = mount(<App />)
     expect(component).toBeDefined()
     expect(component.find('.App-header').text()).toBe('rLogin Sample App - Basic dApp')
--- basic-dapp/src/setupTests.js
+++ basic-dapp/src/setupTests.js
@@ -1,4 +1,8 @@
 import Enzyme from 'enzyme'
 import UnofficialSeventeenAdapter from '@wojtekmaj/enzyme-adapter-react-17'
+import { TextEncoder, TextDecoder } from 'util'
 
 Enzyme.configure({ adapter: new UnofficialSeventeenAdapter() })
+
+global.TextEncoder = TextEncoder
+global.TextDecoder = TextDecoder
